6b902565c5371704d627fc52955fbaa9a01ccee0
Kamil Janek <kjanek@unitygroup.com>
2023-10-01 08:19:17 +0200

    delete

---
+++ HackYeah/Application/Commands/DeleteMissingPetCommand.cs
@@ -0,0 +1,31 @@
+﻿using HackYeah.DAL;
+using HackYeah.DAL.Models;
+using MediatR;
+
+namespace HackYeah.Application.Commands;
+
+public class DeleteMissingPetCommand : IRequest<Guid>
+{
+    public Guid MissingPetReportId { get; set; }
+}
+
+public class DeleteMissingPetCommandHandler : IRequestHandler<DeleteMissingPetCommand, Guid>
+{
+    private readonly HackYeahDbContext _dbContext;
+
+    public DeleteMissingPetCommandHandler(HackYeahDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Guid> Handle(DeleteMissingPetCommand request, CancellationToken cancellationToken)
+    {
+
+        var entityToRemove = _dbContext.MissingPetsReports.FirstOrDefault(e => e.Id == request.MissingPetReportId);
+
+        _dbContext.MissingPetsReports.Remove(entityToRemove);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return entityToRemove.Id;
+    }
+}
