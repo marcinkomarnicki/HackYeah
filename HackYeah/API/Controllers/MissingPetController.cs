6858c0a03499a1466550ba10f37720ecbf316221
Kamil Janek <kjanek@unitygroup.com>
2023-10-01 03:33:12 +0200

    add controller

---
+++ HackYeah/API/Controllers/MissingPetController.cs
@@ -0,0 +1,27 @@
+﻿using HackYeah.Application.Commands;
+using HackYeah.Application.Queries;
+using HackYeah.Application.Queries.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackYeah.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MissingPetController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public MissingPetController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<Guid> GetMissingEncounter(ReportEncounterCommand input)
+    {
+        var id = await _mediator.Send(input);
+
+        return id;
+    }
+}
