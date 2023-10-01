5aba8e7d4d4ebdb1aebc4e292c7f6409b484bf7c
Kamil Janek <kjanek@unitygroup.com>
2023-10-01 07:33:05 +0200

    DASF

---
+++ HackYeah/API/Controllers/FactController.cs
@@ -0,0 +1,30 @@
+﻿using HackYeah.Application.Commands;
+using HackYeah.Application.Queries;
+using HackYeah.Application.Queries.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackYeah.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FactController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public FactController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<string> GetFact([FromQuery]string animalType)
+    {
+        var query = new FactQuery()
+        {
+            AnimalType = animalType
+        };
+
+        return await _mediator.Send(query);
+    }
+}
---
+++ HackYeah/Application/Queries/FactQuery.cs
@@ -0,0 +1,30 @@
+﻿using Azure.AI.OpenAI;
+using MediatR;
+
+namespace HackYeah.Application.Queries
+{
+    public class FactQuery : IRequest<string>
+    {
+        public string AnimalType { get; set; }
+    }
+
+    public class FactQueryHandler : IRequestHandler<FactQuery, string>
+    {
+
+        public FactQueryHandler()
+        {
+        }
+
+        public async Task<string> Handle(FactQuery request,
+            CancellationToken cancellationToken)
+        {
+            OpenAIClient openAiClient = new OpenAIClient("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");
+
+            var response = await openAiClient.GetCompletionsAsync("text-davinci-003", $"Napisz mi krótką ciekawostkę o zwierzęciu {request.AnimalType}");
+
+            var result = response.Value.Choices.First();
+
+            return result.Text;
+        }
+    }
+}
